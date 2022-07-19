beef7fb5955ad1eedf4cadfc3781f93b82034348
강세이 <yhs7409@naver.com>
2022-07-20 03:35:08 +0900

    Need to Connect Json And Ranking, Animation

---
+++ app-assets/js/scripts/custom/chart.js
@@ -0,0 +1,35 @@
+// Bar chart
+window.onload = function () {
+  var dataPoints = [];
+
+  var chart = new CanvasJS.Chart("chartContainer", {
+    animationEnabled: true,
+    theme: "light",
+    title: {
+      text: "피해 등록 현황",
+    },
+
+    data: [
+      {
+        type: "column",
+        yValueFormatString: "#,### 건",
+        dataPoints: dataPoints,
+      },
+    ],
+  });
+
+  function addData(data) {
+    for (var i = 0; i < data.length; i++) {
+      dataPoints.push({
+        x: new Date(data[i].date),
+        y: data[i].units,
+      });
+    }
+    chart.render();
+  }
+
+  $.getJSON(
+    "https://canvasjs.com/data/gallery/javascript/daily-sales-data.json",
+    addData
+  );
+};
